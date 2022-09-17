24986b4ccdcb6ed73dd8bf899f5059c57b50cb0e
JuanLaptop <Juanramirogutierrezhiguera@gmail.com>
2022-09-17 12:58:21 -0600

    si

--- Proyecto Graficacion/Traslacion.Designer.cs
+++ Proyecto Graficacion/Traslacion.Designer.cs
@@ -31,6 +31,7 @@
             this.btnDibujar = new System.Windows.Forms.Button();
             this.btnDetener = new System.Windows.Forms.Button();
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // btnDibujar
@@ -60,12 +61,24 @@
             this.simpleButton1.TabIndex = 2;
             this.simpleButton1.Text = "SUPERXD";
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(180, 217);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
             // Traslacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.simpleButton1);
             this.Controls.Add(this.btnDetener);
             this.Controls.Add(this.btnDibujar);
@@ -73,6 +86,7 @@
             this.Text = "Traslacion";
             this.Load += new System.EventHandler(this.Traslacion_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +95,6 @@
         private System.Windows.Forms.Button btnDibujar;
         private System.Windows.Forms.Button btnDetener;
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
