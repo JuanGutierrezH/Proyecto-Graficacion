f0dcc42b166057704bd605347d312c5c3eeb2311
JuanLaptop <Juanramirogutierrezhiguera@gmail.com>
2022-09-19 16:21:25 -0600

    ghosting

--- Proyecto Graficacion/Unidad2/Traslacion.Designer.cs
+++ Proyecto Graficacion/Unidad2/Traslacion.Designer.cs
@@ -57,7 +57,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(1029, 607);
+            this.ClientSize = new System.Drawing.Size(975, 597);
             this.Controls.Add(this.btnDetener);
             this.Controls.Add(this.btnDibujar);
             this.Name = "Traslacion";
--- Proyecto Graficacion/Unidad2/Traslacion.cs
+++ Proyecto Graficacion/Unidad2/Traslacion.cs
@@ -19,16 +19,19 @@ namespace Proyecto_Graficacion
         }
 
         Graphics dibujo;
+        Graphics dibujo2;
         Pen pluma = new Pen(Color.Black, 3);
         Brush brush = new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#7A3EB1"));
 
         Rectangle cuadro1 = new Rectangle(300, 400, 100, 100);
         Rectangle cuadro2 = new Rectangle(450, 400, 100, 100);
         Rectangle cuadro3 = new Rectangle(600, 400, 100, 100);
+        Rectangle ghostCuadro = new Rectangle(2, 2, 1, 1);
 
         private void Traslacion_Load(object sender, EventArgs e)
         {
             dibujo = this.CreateGraphics();
+            dibujo2 = this.CreateGraphics();
         }
 
         private void btnDibujar_Click(object sender, EventArgs e)
@@ -40,27 +43,42 @@ namespace Proyecto_Graficacion
         private void DibujarCuadros()
         {
             int numIteraciones = 50;
-            int dy = 2;
-            int dx = 2;
+            int dy = 4;
+            int dx = 4;
 
             for (int i = 0; i < numIteraciones; i++)
             {
                 dibujo.Clear(System.Drawing.ColorTranslator.FromHtml("#404040"));
                 dibujo.DrawRectangle(pluma, cuadro1);
                 dibujo.DrawRectangle(pluma, cuadro2);
-                dibujo.DrawRectangle(pluma, cuadro3);
+                dibujo2.DrawRectangle(pluma, cuadro3);
+                dibujo.DrawRectangle(pluma, ghostCuadro);
                 Thread.Sleep(20);
 
                 cuadro1 = new Rectangle(cuadro1.X - dx, cuadro1.Y - dy, 100, 100);
                 cuadro2 = new Rectangle(cuadro2.X, cuadro2.Y - dy, 100, 100);
-                cuadro3 = new Rectangle(cuadro3.X + dx, cuadro3.Y - dy, 100, 100);
+                cuadro3 = new Rectangle(cuadro3.X + dx, cuadro3.Y - dy , 100, 100);
+                ghostCuadro = new Rectangle(ghostCuadro.X + dx, ghostCuadro.Y, 1, 1);
+            }
+            for (int j = 0; j < numIteraciones; j++)
+            {
+                dibujo.Clear(System.Drawing.ColorTranslator.FromHtml("#404040"));
+                dibujo.DrawRectangle(pluma, cuadro1);
+                dibujo.DrawRectangle(pluma, cuadro2);
+                dibujo2.DrawRectangle(pluma, cuadro3);
+                dibujo.DrawRectangle(pluma, ghostCuadro);
+
+                Thread.Sleep(20);
+
+                cuadro1 = new Rectangle(cuadro1.X + dx, cuadro1.Y + dy, 100, 100);
+                cuadro2 = new Rectangle(cuadro2.X, cuadro2.Y + dy, 100, 100);
+                cuadro3 = new Rectangle(cuadro3.X - dx, cuadro3.Y + dy, 100, 100);
+                ghostCuadro = new Rectangle(ghostCuadro.X + dx, ghostCuadro.Y, 1, 1);
             }
         }
 
         private void btnDetener_Click(object sender, EventArgs e)
         {
-            dibujo.SetClip(new Rectangle(300, 400, 100, 100));
-            dibujo.TranslateClip(100, 100);
         }
     }
 }
