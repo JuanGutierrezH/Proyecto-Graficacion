0f80f94dcbd412b1848e7286ed325671028491d1
JuanDesktop <Juanramirogutierrezhiguera@gmail.com>
2022-10-05 23:44:17 -0600

    ya dibuja

--- Proyecto Graficacion/Unidad2/Paint.Designer.cs
+++ Proyecto Graficacion/Unidad2/Paint.Designer.cs
@@ -34,6 +34,7 @@
             this.applicationMenu1 = new DevExpress.XtraBars.Ribbon.ApplicationMenu(this.components);
             this.btnNew = new DevExpress.XtraBars.BarButtonItem();
             this.btnOpen = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem10 = new DevExpress.XtraBars.BarButtonItem();
             this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
             this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
             this.barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
@@ -44,6 +45,15 @@
             this.barButtonItem6 = new DevExpress.XtraBars.BarButtonItem();
             this.PaletaColores2 = new DevExpress.XtraBars.BarEditItem();
             this.colorEdit2 = new DevExpress.XtraEditors.Repository.RepositoryItemColorPickEdit();
+            this.barButtonItem7 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem8 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem9 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem11 = new DevExpress.XtraBars.BarButtonItem();
+            this.PaletaColores3 = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemColorEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemColorEdit();
+            this.plumaTamano = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemSpinEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemSpinEdit();
+            this.ribbonPageCategory1 = new DevExpress.XtraBars.Ribbon.RibbonPageCategory();
             this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
             this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
@@ -51,12 +61,17 @@
             this.ribbonPage2 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
             this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
-            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.ribbonPage5 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tamano = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.applicationMenu1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.colorEdit)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.colorEdit2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemColorEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemSpinEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tamano)).BeginInit();
             this.SuspendLayout();
             // 
             // ribbon
@@ -75,16 +90,27 @@
             this.PaletaColores,
             this.barButtonItem5,
             this.barButtonItem6,
-            this.PaletaColores2});
+            this.PaletaColores2,
+            this.barButtonItem7,
+            this.barButtonItem8,
+            this.barButtonItem9,
+            this.barButtonItem10,
+            this.barButtonItem11,
+            this.PaletaColores3,
+            this.plumaTamano});
             this.ribbon.Location = new System.Drawing.Point(0, 0);
-            this.ribbon.MaxItemId = 11;
+            this.ribbon.MaxItemId = 18;
             this.ribbon.Name = "ribbon";
+            this.ribbon.PageCategories.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageCategory[] {
+            this.ribbonPageCategory1});
             this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1,
             this.ribbonPage2});
             this.ribbon.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
             this.colorEdit,
-            this.colorEdit2});
+            this.colorEdit2,
+            this.repositoryItemColorEdit1,
+            this.repositoryItemSpinEdit1});
             this.ribbon.Size = new System.Drawing.Size(828, 158);
             this.ribbon.StatusBar = this.ribbonStatusBar;
             // 
@@ -92,6 +118,7 @@
             // 
             this.applicationMenu1.ItemLinks.Add(this.btnNew);
             this.applicationMenu1.ItemLinks.Add(this.btnOpen);
+            this.applicationMenu1.ItemLinks.Add(this.barButtonItem10);
             this.applicationMenu1.Name = "applicationMenu1";
             this.applicationMenu1.Ribbon = this.ribbon;
             // 
@@ -108,6 +135,16 @@
             this.btnOpen.Id = 3;
             this.btnOpen.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.ImageOptions.Image")));
             this.btnOpen.Name = "btnOpen";
+            this.btnOpen.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnOpen_ItemClick);
+            // 
+            // barButtonItem10
+            // 
+            this.barButtonItem10.Caption = "Guardar";
+            this.barButtonItem10.Id = 14;
+            this.barButtonItem10.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem10.ImageOptions.Image")));
+            this.barButtonItem10.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barButtonItem10.ImageOptions.LargeImage")));
+            this.barButtonItem10.Name = "barButtonItem10";
+            this.barButtonItem10.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem10_ItemClick);
             // 
             // barButtonItem1
             // 
@@ -142,7 +179,6 @@
             this.PaletaColores.Id = 7;
             this.PaletaColores.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("PaletaColores.ImageOptions.SvgImage")));
             this.PaletaColores.Name = "PaletaColores";
-            this.PaletaColores.EditValueChanged += new System.EventHandler(this.PaletaColores_EditValueChanged);
             // 
             // colorEdit
             // 
@@ -179,6 +215,64 @@
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
             this.colorEdit2.Name = "colorEdit2";
             // 
+            // barButtonItem7
+            // 
+            this.barButtonItem7.Id = 11;
+            this.barButtonItem7.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("barButtonItem7.ImageOptions.SvgImage")));
+            this.barButtonItem7.Name = "barButtonItem7";
+            // 
+            // barButtonItem8
+            // 
+            this.barButtonItem8.Id = 12;
+            this.barButtonItem8.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("barButtonItem8.ImageOptions.SvgImage")));
+            this.barButtonItem8.Name = "barButtonItem8";
+            // 
+            // barButtonItem9
+            // 
+            this.barButtonItem9.Caption = "barButtonItem9";
+            this.barButtonItem9.Id = 13;
+            this.barButtonItem9.Name = "barButtonItem9";
+            // 
+            // barButtonItem11
+            // 
+            this.barButtonItem11.Id = 15;
+            this.barButtonItem11.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("barButtonItem11.ImageOptions.SvgImage")));
+            this.barButtonItem11.Name = "barButtonItem11";
+            this.barButtonItem11.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem11_ItemClick);
+            // 
+            // PaletaColores3
+            // 
+            this.PaletaColores3.Caption = "Color";
+            this.PaletaColores3.Edit = this.repositoryItemColorEdit1;
+            this.PaletaColores3.Id = 16;
+            this.PaletaColores3.Name = "PaletaColores3";
+            this.PaletaColores3.EditValueChanged += new System.EventHandler(this.barEditItem1_EditValueChanged);
+            // 
+            // repositoryItemColorEdit1
+            // 
+            this.repositoryItemColorEdit1.AutoHeight = false;
+            this.repositoryItemColorEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemColorEdit1.Name = "repositoryItemColorEdit1";
+            // 
+            // plumaTamano
+            // 
+            this.plumaTamano.Edit = this.repositoryItemSpinEdit1;
+            this.plumaTamano.Id = 17;
+            this.plumaTamano.Name = "plumaTamano";
+            // 
+            // repositoryItemSpinEdit1
+            // 
+            this.repositoryItemSpinEdit1.AutoHeight = false;
+            this.repositoryItemSpinEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemSpinEdit1.Name = "repositoryItemSpinEdit1";
+            // 
+            // ribbonPageCategory1
+            // 
+            this.ribbonPageCategory1.Name = "ribbonPageCategory1";
+            this.ribbonPageCategory1.Text = "ribbonPageCategory1";
+            // 
             // ribbonPage1
             // 
             this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
@@ -199,15 +293,19 @@
             // ribbonPageGroup2
             // 
             this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem2);
-            this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem3);
+            this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem11);
             this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem4);
+            this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem8);
+            this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem7);
+            this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem3);
+            this.ribbonPageGroup2.ItemsLayout = DevExpress.XtraBars.Ribbon.RibbonPageGroupItemsLayout.ThreeRows;
             this.ribbonPageGroup2.Name = "ribbonPageGroup2";
-            this.ribbonPageGroup2.Text = "ribbonPageGroup2";
+            this.ribbonPageGroup2.State = DevExpress.XtraBars.Ribbon.RibbonPageGroupState.Expanded;
+            this.ribbonPageGroup2.Text = "Dibujar";
             // 
             // ribbonPageGroup4
             // 
-            this.ribbonPageGroup4.ItemLinks.Add(this.PaletaColores);
-            this.ribbonPageGroup4.ItemLinks.Add(this.PaletaColores2);
+            this.ribbonPageGroup4.ItemLinks.Add(this.PaletaColores3);
             this.ribbonPageGroup4.Name = "ribbonPageGroup4";
             this.ribbonPageGroup4.Text = "Colores";
             // 
@@ -230,31 +328,54 @@
             this.ribbonStatusBar.Ribbon = this.ribbon;
             this.ribbonStatusBar.Size = new System.Drawing.Size(828, 24);
             // 
-            // panelControl1
+            // ribbonPage5
             // 
-            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelControl1.Location = new System.Drawing.Point(0, 158);
-            this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(828, 360);
-            this.panelControl1.TabIndex = 2;
+            this.ribbonPage5.Name = "ribbonPage5";
+            this.ribbonPage5.Text = "ribbonPage5";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tamano
+            // 
+            this.tamano.Location = new System.Drawing.Point(189, 111);
+            this.tamano.Name = "tamano";
+            this.tamano.Size = new System.Drawing.Size(32, 21);
+            this.tamano.TabIndex = 2;
+            this.tamano.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.tamano.ValueChanged += new System.EventHandler(this.tamano_ValueChanged);
             // 
             // Paint
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(828, 542);
-            this.Controls.Add(this.panelControl1);
+            this.Controls.Add(this.tamano);
             this.Controls.Add(this.ribbonStatusBar);
             this.Controls.Add(this.ribbon);
             this.Name = "Paint";
             this.Ribbon = this.ribbon;
             this.StatusBar = this.ribbonStatusBar;
             this.Text = "Paint";
+            this.Load += new System.EventHandler(this.Paint_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Paint_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Paint_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Paint_MouseDown);
+            this.MouseLeave += new System.EventHandler(this.Paint_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Paint_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Paint_MouseUp);
             ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.applicationMenu1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.colorEdit)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.colorEdit2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemColorEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemSpinEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tamano)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -276,7 +397,6 @@
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
         private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage2;
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
-        private DevExpress.XtraEditors.PanelControl panelControl1;
         private DevExpress.XtraBars.BarEditItem PaletaColores;
         private DevExpress.XtraEditors.Repository.RepositoryItemColorEdit colorEdit;
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup4;
@@ -284,5 +404,19 @@
         private DevExpress.XtraBars.BarButtonItem barButtonItem6;
         private DevExpress.XtraBars.BarEditItem PaletaColores2;
         private DevExpress.XtraEditors.Repository.RepositoryItemColorPickEdit colorEdit2;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem7;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem8;
+        private DevExpress.XtraBars.Ribbon.RibbonPageCategory ribbonPageCategory1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage5;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem10;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem9;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem11;
+        private DevExpress.XtraBars.BarEditItem PaletaColores3;
+        private DevExpress.XtraEditors.Repository.RepositoryItemColorEdit repositoryItemColorEdit1;
+        private DevExpress.XtraBars.BarEditItem plumaTamano;
+        private DevExpress.XtraEditors.Repository.RepositoryItemSpinEdit repositoryItemSpinEdit1;
+        private System.Windows.Forms.NumericUpDown tamano;
     }
 }
--- Proyecto Graficacion/Unidad2/Paint.cs
+++ Proyecto Graficacion/Unidad2/Paint.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.Data.Controls.ExpressionEditor;
+using DevExpress.Utils.CommonDialogs;
 using DevExpress.XtraBars;
 using System;
 using System.Collections.Generic;
@@ -14,9 +15,12 @@ namespace Proyecto_Graficacion.Unidad2
 {
     public partial class Paint : DevExpress.XtraBars.Ribbon.RibbonForm
     {
-        Color color = Color.Black;
-        Pen pluma = new Pen(color, 3);
-        Brush brush = new SolidBrush(color);
+        Pen pluma = new Pen(ColorTranslator.FromHtml("#F0F0F0"), 0);
+        Brush brush = new SolidBrush(Color.Black);
+        Image ImagenLapiz;
+        bool clicked = false;
+        Point previousPoint;
+        bool clickRectiangulo;
 
 
         public Paint()
@@ -26,13 +30,90 @@ namespace Proyecto_Graficacion.Unidad2
 
         private void barButtonItem2_ItemClick(object sender, ItemClickEventArgs e)
         {
-            MessageBox.Show("Que rollo");
+            pluma = new Pen(Color.Black, (float)tamano.Value);
         }
-
-        private void PaletaColores_EditValueChanged(object sender, EventArgs e)
+        
+        private void PaletaColores2_EditValueChanged(object sender, EventArgs e)
         {
-            color = (Color)(PaletaColores.EditValue);
+            pluma = new Pen((Color)(PaletaColores2.EditValue), 3);
+            //color = (Color)(PaletaColores2.EditValue);
+            //pluma.Color = color;
         }
 
+        private void btnOpen_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string ruta = openFileDialog1.FileName;
+        }
+
+        private void barButtonItem10_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            string ruta = saveFileDialog1.FileName;
+
+            ImagenLapiz.Save(ruta);
+        }
+
+        private void Paint_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Paint_Paint(object sender, PaintEventArgs e)
+        {
+            if (ImagenLapiz != null)
+                e.Graphics.DrawImage(ImagenLapiz, 0, 0);
+        }
+
+        private void Paint_MouseDown(object sender, MouseEventArgs e)
+        {
+            clicked = true;
+            previousPoint = e.Location;
+        }
+
+        private void Paint_MouseLeave(object sender, EventArgs e)
+        {
+            clicked = false;
+        }
+
+        private void Paint_MouseUp(object sender, MouseEventArgs e)
+        {
+            clicked = false;
+        }
+
+        private void Paint_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clicked)
+            {
+                if (ImagenLapiz == null)
+                    ImagenLapiz = new Bitmap(this.Width, this.Height);
+                using (Graphics g = Graphics.FromImage(ImagenLapiz))
+                {
+                    g.DrawLine(pluma, previousPoint, e.Location);
+                    previousPoint = e.Location;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        private void barEditItem1_EditValueChanged(object sender, EventArgs e)
+        {
+            pluma = new Pen( (Color)(PaletaColores3.EditValue), (float)tamano.Value);
+        }
+
+        private void barButtonItem11_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            pluma = new Pen(ColorTranslator.FromHtml("#F0F0F0"), 20);
+        }
+
+        private void tamano_ValueChanged(object sender, EventArgs e)
+        {
+            pluma = new Pen((Color)(PaletaColores3.EditValue), (float)tamano.Value);
+        }
+
+        private void Paint_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
     }
 }
